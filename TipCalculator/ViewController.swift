40927a2e5630ba1794b9f8563999c3ef05ad53b9
Robert Mitchell <robertmitchell@Roberts-MacBook-Pro.local>
2017-01-04 00:36:35 -0800

    added gitignore, and attributes, and all swift files

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Robert Mitchell on 1/3/17.
+//  Copyright © 2017 Robert Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var billField: UITextField!
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+        
+    }
+
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18 , 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+    }
+    
+    
+}
+
